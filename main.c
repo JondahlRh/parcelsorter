eacb9c8d10b878f35a8da4a0cfd6b4b89ba04c35
Jondahl Rhenius <jondahlrh@gmail.com>
2026-06-16 16:52:03 +0200

    Add global definitions and variables

--- main.c
+++ main.c
@@ -5,6 +5,30 @@
 #include <rtai_sched.h>
 #include <rtai_sem.h>
 
+// rtai definitions and variables
+#define RTAI_ADDRESS 0xC000
+int rtaiBitmuster = 0x00;
+SEM semRtaiBitmuster;
+
+#define BELT_1 0x01
+#define BELT_2 0x02
+#define EJECTOR_1 0x04
+#define EJECTOR_2 0x08
+#define EJECTOR_3 0x10
+#define BARCODE_SCANNER 0x80
+
+// parcel tracking definitions and variables
+#define NUMBER_OF_REGIONS 8
+int parcelTrackingData[NUMBER_OF_REGIONS];
+SEM semParcelTrackingData;
+
+// light barrier definitions and variables
+#define LIGHT_BARRIER_1 0x01
+#define LIGHT_BARRIER_2 0x40
+#define LIGHT_BARRIER_3 0x04
+#define LIGHT_BARRIER_4 0x08
+#define LIGHT_BARRIER_5 0x10
+
 static __init int parallel_init(void) {
   rt_printk("__ init __");
   return 0;
