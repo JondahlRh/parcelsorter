ee1288d9cb6f96706a315d30b57433e1057b1aac
Jondahl Rhenius <jondahlrh@gmail.com>
2026-06-17 10:24:19 +0200

    remove test task

--- main.c
+++ main.c
@@ -84,14 +84,14 @@ void toggle(int value) {
 inline int readLightBarriers(void) { return inb(RTAI_ADDRESS + 4); }
 
 // test task
-RT_TASK rtTestTask;
-void testTask(long i) {
-  while (1) {
-    rt_printk("test task running...");
-    rt_sleep(nano2count(1000 * 1000 * 1000));
-    rt_task_wait_period();
-  }
-}
+// RT_TASK rtTestTask;
+// void testTask(long i) {
+//   while (1) {
+//     rt_printk("test task running...");
+//     rt_sleep(nano2count(1000 * 1000 * 1000));
+//     rt_task_wait_period();
+//   }
+// }
 
 RT_TASK rtLightBarrierCheckTask;
 void lightBarrierCheckTask(long i) {
@@ -148,7 +148,7 @@ static __init int parallel_init(void) {
 
   rt_task_init(&rtLightBarrierCheckTask, lightBarrierCheckTask, 0x00, 3000, 4,
                0, 0);
-  rt_task_init(&rtTestTask, testTask, 0x00, 3000, 4, 0, 0);
+  // rt_task_init(&rtTestTask, testTask, 0x00, 3000, 4, 0, 0);
 
   rt_typed_sem_init(&semRtaiBitmuster, 1, RES_SEM);
   rt_typed_sem_init(&semParcelTrackingData, 1, RES_SEM);
@@ -160,7 +160,7 @@ static __init int parallel_init(void) {
   RTIME tstart = rt_get_time() + nano2count(10 * 1000 * 1000);
   rt_task_make_periodic(&rtLightBarrierCheckTask, tstart,
                         nano2count(240000000));
-  rt_task_make_periodic(&rtTestTask, tstart, nano2count(240000000));
+  // rt_task_make_periodic(&rtTestTask, tstart, nano2count(240000000));
 
   rt_printk("__ init __");
   return 0;
@@ -176,7 +176,7 @@ static __exit void parallel_exit(void) {
   rt_sem_delete(&semLightBarriersData);
 
   rt_task_delete(&rtLightBarrierCheckTask);
-  rt_task_delete(&rtTestTask);
+  // rt_task_delete(&rtTestTask);
 
   rt_umount();
 
