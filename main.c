587f42ff566ccf3c0d6efc16943b735d569e59d6
Jondahl Rhenius <jondahlrh@gmail.com>
2026-06-17 12:43:46 +0200

    ejection task

--- main.c
+++ main.c
@@ -31,10 +31,11 @@ SEM semLightBarriersData;
 #define LIGHT_BARRIER_3 0x04
 #define LIGHT_BARRIER_4 0x08
 #define LIGHT_BARRIER_5 0x10
+int lightBarriersIndexMap[5] = {LIGHT_BARRIER_2, LIGHT_BARRIER_3,
+                                LIGHT_BARRIER_4, LIGHT_BARRIER_5};
 
-// general helper functions
 /**
- * Convert a binary number to a string (for debugging purposes)
+ * Convert a byte to a string (for debugging purposes)
  */
 void byteToString(char buffer[], int value) {
   int i;
@@ -49,6 +50,9 @@ void byteToString(char buffer[], int value) {
   buffer[8] = '\0';
 }
 
+/**
+ * Convert an integer array to a string (for debugging purposes)
+ */
 void intArrayToString(char buffer[], int value[], int length) {
   int i;
   for (i = 0; i < length; i++) {
@@ -91,7 +95,10 @@ void toggle(int value) {
 inline int readLightBarriers(void) { return inb(RTAI_ADDRESS + 4); }
 
 /**
- * Get the value of a light barrier at provided bit if it has changed
+ * Get the value of a light barrier at provided bit
+ * - -1: has not change
+ * - 0: changed and now active
+ * - 1: changed and now inactive
  */
 int getLightBarrier(int bit, int newData, int oldData) {
   if ((newData & bit) == (oldData & bit)) return -1;
@@ -99,12 +106,18 @@ int getLightBarrier(int bit, int newData, int oldData) {
 }
 
 // parcel tracking helper functions
+/**
+ * Add parcel at first position with the id of the exjection position
+ */
 void addParcelToTracking(int parcel) {
   rt_sem_wait(&semParcelTrackingData);
   parcelTrackingData[0] = parcel;
   rt_sem_signal(&semParcelTrackingData);
 }
 
+/**
+ * Transfer parcel tracking data from one region to the next
+ */
 void transferParcelTrackingRegion(int index) {
   rt_sem_wait(&semParcelTrackingData);
   if ((index + 1) < NUMBER_OF_REGIONS) {
@@ -114,23 +127,25 @@ void transferParcelTrackingRegion(int index) {
   rt_sem_signal(&semParcelTrackingData);
 }
 
-int lightBarrierBitToIndexMap[5] = {LIGHT_BARRIER_2, LIGHT_BARRIER_3,
-                                    LIGHT_BARRIER_4, LIGHT_BARRIER_5};
-
-RT_TASK rtLightBarrierCheckTask;
-void lightBarrierCheckTask(long i) {
+// light barrier task:
+// check if light barriers have changed and transfer parcel tracking data
+RT_TASK rtLightBarrierTask;
+void lightBarrierTask(long i) {
   int newValue, oldValue, currentValue, index;
   char buffer[9];
 
   activate(BELT_1 + BELT_2);
 
   while (1) {
+    // get new light barrier data
     newValue = readLightBarriers();
 
+    // get internal (old) light barrier data
     rt_sem_wait(&semLightBarriersData);
     oldValue = lightBarriersData;
     rt_sem_signal(&semLightBarriersData);
 
+    // debugging
     byteToString(buffer, newValue);
     rt_printk("light barriers new:      %s", buffer);
     byteToString(buffer, lightBarriersData);
@@ -138,6 +153,7 @@ void lightBarrierCheckTask(long i) {
     intArrayToString(buffer, parcelTrackingData, NUMBER_OF_REGIONS);
     rt_printk("parcel tracking data:    %s", buffer);
 
+    // check if light barrier 1 has changed
     currentValue = getLightBarrier(LIGHT_BARRIER_1, newValue, oldValue);
     if (currentValue == 0) {
       // TODO: scanner
@@ -148,15 +164,17 @@ void lightBarrierCheckTask(long i) {
       activate(BELT_1);
     }
 
+    // check if light barriers (after first) have changed
     for (index = 0; index < 5; index++) {
       currentValue =
-          getLightBarrier(lightBarrierBitToIndexMap[index], newValue, oldValue);
+          getLightBarrier(lightBarriersIndexMap[index], newValue, oldValue);
       if (currentValue == 0) {
         transferParcelTrackingRegion(index * 2);
       } else if (currentValue == 1) {
         transferParcelTrackingRegion(index * 2 + 1);
       }
 
+      // update internal light barrier data
       rt_sem_wait(&semLightBarriersData);
       lightBarriersData = newValue;
       rt_sem_signal(&semLightBarriersData);
@@ -167,11 +185,22 @@ void lightBarrierCheckTask(long i) {
   }
 }
 
+RT_TASK rtEjectionTask;
+void ejectionTask(long i) {
+  while (1) {
+    // debugging
+    rt_printk("ejection task running...");
+
+    rt_sleep(nano2count(500 * 1000 * 1000));
+    rt_task_wait_period();
+  }
+}
+
 static __init int parallel_init(void) {
   rt_mount();
 
-  rt_task_init(&rtLightBarrierCheckTask, lightBarrierCheckTask, 0x00, 3000, 4,
-               0, 0);
+  rt_task_init(&rtLightBarrierTask, lightBarrierTask, 0x00, 3000, 4, 0, 0);
+  rt_task_init(&rtEjectionTask, ejectionTask, 0x00, 3000, 4, 0, 0);
 
   rt_typed_sem_init(&semRtaiBitmuster, 1, RES_SEM);
   rt_typed_sem_init(&semParcelTrackingData, 1, RES_SEM);
@@ -180,9 +209,12 @@ static __init int parallel_init(void) {
   rt_set_periodic_mode();
   start_rt_timer(0);
 
-  RTIME tstart = rt_get_time() + nano2count(10 * 1000 * 1000);
-  rt_task_make_periodic(&rtLightBarrierCheckTask, tstart,
-                        nano2count(120000000));
+  RTIME tstart1, tstart2;
+  tstart1 = rt_get_time() + nano2count(1000 * 1000);
+  tstart2 = rt_get_time() + nano2count(100 * 1000 * 1000);
+
+  rt_task_make_periodic(&rtLightBarrierTask, tstart1, nano2count(120000000));
+  rt_task_make_periodic(&rtEjectionTask, tstart2, nano2count(120000000));
 
   rt_printk("__ init __");
   return 0;
@@ -197,7 +229,8 @@ static __exit void parallel_exit(void) {
   rt_sem_delete(&semParcelTrackingData);
   rt_sem_delete(&semLightBarriersData);
 
-  rt_task_delete(&rtLightBarrierCheckTask);
+  rt_task_delete(&rtLightBarrierTask);
+  rt_task_delete(&rtEjectionTask);
 
   rt_umount();
 
