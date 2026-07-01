f6a1c36f33a2ec2e754360e893736c94fb5088af
Jondahl Rhenius <jondahlrh@gmail.com>
2026-07-01 12:03:15 +0200

    small (big) improvments

--- parcelsorter.c
+++ parcelsorter.c
@@ -7,6 +7,12 @@
 #include <rtai_sched.h>
 #include <rtai_sem.h>
 
+// base definitions
+#define RT_STACK_SIZE 4096
+#define RT_HIGH_PRIORITY 0
+#define RT_MEDIUM_PRIORITY 1
+#define RT_LOW_PRIORITY 2
+
 // address of the rtai module
 #define RTAI_ADDRESS 0xC000
 
@@ -113,14 +119,23 @@ void toggle(int value) {
 void addNewParcelToTrackingData(int scannerValue) {
   int ejectionId;
 
-  if (scannerValue == 1) ejectionId = 1;
-  if (scannerValue == 4) ejectionId = 2;
-  if (scannerValue == 9) ejectionId = 3;
+  switch (scannerValue) {
+    case 1:
+      ejectionId = 1;
+      break;
+    case 4:
+      ejectionId = 2;
+      break;
+    case 9:
+      ejectionId = 3;
+      break;
+    default:
+      ejectionId = 0;
+      break;
+  }
 
   rt_sem_wait(&sem_parcelTrackingData);
-
   parcelTrackingData[0] = ejectionId;
-
   rt_sem_signal(&sem_parcelTrackingData);
 }
 
@@ -163,38 +178,42 @@ void task_readAndUpdateLightBarriers(long i) {
   char buffer[9];
 
   while (true) {
-    // read the current state of the light barriers
+    // read light barriers und update internal state
+    // when nothing has changed, wait and continue
     newValue = readLightBarriers();
 
-    // read und update internal state of light barriers
     rt_sem_wait(&sem_internalLightBarriersData);
     oldValue = internalLightBarriersData;
     internalLightBarriersData = newValue;
     rt_sem_signal(&sem_internalLightBarriersData);
 
-    // if nothing has changed, wait and continue
     if (newValue == oldValue) {
       rt_task_wait_period();
       continue;
     };
 
+    // ckeck first light barrier
     if ((newValue & LIGHT_BARRIERS[0]) != (oldValue & LIGHT_BARRIERS[0])) {
       if ((newValue & LIGHT_BARRIERS[0]) == 0) {
+        // parcel entered first light barrier
         activate(BARCODE_SCANNER);
         deactivate(BELTS[0]);
       } else {
+        // parcel left first light barrier
         deactivate(BARCODE_SCANNER);
         activate(BELTS[1]);
       }
     }
 
+    // check other light barriers
     for (idx = 1; idx < NUMBER_OF_LIGHT_BARRIERS; idx++) {
-      // ignore if the light barrier has not changed
       if ((newValue & LIGHT_BARRIERS[idx]) ==
           (oldValue & LIGHT_BARRIERS[idx])) {
+        // light barrier has not changed, continue to next light barrier
         continue;
       }
 
+      // trigger move parcel task
       parcelTrackingIndex = idx * 2 + (newValue & LIGHT_BARRIERS[idx]) - 1;
       rt_mbx_send(&mailbox_moveParcel, &parcelTrackingIndex, sizeof(int));
     }
@@ -213,6 +232,7 @@ void task_moveParcel(long i) {
   while (true) {
     rt_mbx_receive(&mailbox_moveParcel, &parcelTrackingIndex, sizeof(int));
 
+    // get parcel data and move it to the next region
     rt_sem_wait(&sem_parcelTrackingData);
     parcelEjectionId = parcelTrackingData[parcelTrackingIndex];
     if ((parcelTrackingIndex + 1) < NUMBER_OF_PARCEL_REGIONS) {
@@ -221,8 +241,10 @@ void task_moveParcel(long i) {
     parcelTrackingData[parcelTrackingIndex] = 0;
     rt_sem_signal(&sem_parcelTrackingData);
 
+    // check if parcel is at a ejection region and it matches the parcel data
     if (parcelTrackingIndex % 2 == 0 &&
         parcelEjectionId == parcelTrackingIndex / 2) {
+      // trigger eject parcel task
       rt_mbx_send(&mailbox_ejectParcel, &parcelEjectionId, sizeof(int));
     }
   }
@@ -240,22 +262,23 @@ void task_ejectParcel(long i) {
     rt_mbx_receive(&mailbox_ejectParcel, &parcelEjectionId, sizeof(int));
 
     // minimum sleep before ejecting can be safe
-    rt_sleep(nano2count(100 * 1000 * 1000));  // TODO: RTIME
+    rt_sleep(nano2count(200 * 1000 * 1000));
 
-    maximumWaitIndex = 200;
-    while (true) {
+    // check if ejection is safe
+    // maximum wait time is 1.1 seconds, after that ejection will be ignored
+    maximumWaitIndex = 11;
+    while (maximumWaitIndex > 0) {
       isEjectionSave = isEjectionSaveAtIndex((parcelEjectionId + 1) * 2);
-      if (!isEjectionSave) {
-        maximumWaitIndex--;
-        rt_sleep(nano2count(10 * 1000 * 1000));  // TODO: RTIME
-        continue;
-      }
+      if (isEjectionSave) break;
 
-      break;
+      maximumWaitIndex--;
+      rt_sleep(nano2count(100 * 1000 * 1000));
+      continue;
     }
 
     if (!isEjectionSave) continue;
 
+    // eject parcel
     activate(EJECTORS[parcelEjectionId - 1]);
     rt_sleep(nano2count(500 * 1000 * 1000));
     deactivate(EJECTORS[parcelEjectionId - 1]);
@@ -272,16 +295,18 @@ int fifo_readScannerData(int i) {
   fifoReturnValue = rtf_get(FIFO_NUMBER, buffer, FIFO_SIZE);
   if (fifoReturnValue == 0) return 0;
 
+  // set invalid values to 0
   firstChar = buffer[1];
   if (firstChar > '9' || firstChar < '0') {
     firstChar = '0';
   }
 
+  // add new parcel to tracking data (as an int)
   addNewParcelToTrackingData(firstChar - '0');
   return 0;
 }
 
-RTIME timer, tstart;
+RTIMRE t_baseTimer, t_startLightBarrierTask, t_timerLightBarrierTask;
 static __init int parallel_init(void) {
   rt_printk("Parcelsorter: Initializing");
 
@@ -290,10 +315,19 @@ static __init int parallel_init(void) {
   setRtaiBitmuster(0x00);
   activate(BELTS[0] + BELTS[1]);
 
-  rt_task_init(&rttask_readAndUpdateLightBarriers,
-               task_readAndUpdateLightBarriers, 0x00, 3000, 3, 0, 0);
-  rt_task_init(&rttask_moveParcel, task_moveParcel, 0x00, 3000, 2, 0, 0);
-  rt_task_init(&rttask_ejectParcel, task_ejectParcel, 0x00, 3000, 1, 0, 0);
+  rt_task_init(
+      &rttask_readAndUpdateLightBarriers, task_readAndUpdateLightBarriers, 0,
+      RT_STACK_SIZE,
+      HIGH_PRIORITY,  // highest priority to check for light barrier changes
+      0, 0);
+  rt_task_init(
+      &rttask_moveParcel, task_moveParcel, 0, RT_STACK_SIZE,
+      MEDIUM_PRIORITY,  // medium priority to move parcels in internal state
+      0, 0);
+  rt_task_init(
+      &rttask_ejectParcel, task_ejectParcel, 0, RT_STACK_SIZE,
+      LOW_PRIORITY,  // lowest  priority to eject parcels from the belts
+      0, 0);
 
   rtf_create(FIFO_NUMBER, FIFO_SIZE);
   rtf_create_handler(FIFO_NUMBER, &fifo_readScannerData);
@@ -305,18 +339,18 @@ static __init int parallel_init(void) {
   rt_typed_mbx_init(&mailbox_moveParcel, MAILBOX_SIZE, FIFO_Q);
   rt_typed_mbx_init(&mailbox_ejectParcel, MAILBOX_SIZE, FIFO_Q);
 
-  // TODO: RTIME
-  timer = nano2count(100 * 1000 * 1000);
+  t_baseTimer = nano2count(100 * 1000 * 1000);
 
   rt_set_periodic_mode();
-  start_rt_timer(timer);
+  start_rt_timer(t_baseTimer);
 
-  // TODO: RTIME
-  tstart = rt_get_time() + nano2count(100 * 1000 * 1000);
+  t_startLightBarrierTask = rt_get_time() + nano2count(500 * 1000 * 1000);
+  t_timerLightBarrierTask = nano2count(500 * 1000 * 1000);
 
-  rt_task_make_periodic(&rttask_readAndUpdateLightBarriers, tstart, timer);
-  rt_task_make_periodic(&rttask_moveParcel, tstart, timer);
-  rt_task_make_periodic(&rttask_ejectParcel, tstart, timer);
+  rt_task_make_periodic(&rttask_readAndUpdateLightBarriers,
+                        t_startLightBarrierTask, t_timerLightBarrierTask);
+  rt_task_resume(&rttask_moveParcel);
+  rt_task_resume(&rttask_ejectParcel);
 
   return 0;
 }
